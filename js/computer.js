b61664d4c48020be9a17ead1dbf10d76706b7dac
Rubyfr0stbite <85445566+Rubyfr0stbite@users.noreply.github.com>
2021-09-26 12:23:53 +0100

    Add files via upload

---
+++ js/computer.js
@@ -0,0 +1,34 @@
+class computer{
+
+    constructor(x,y,w,h){
+   
+       var options = {
+         isStatic : true
+       }
+
+       this.body = Bodies.rectangle(x,y,w,h, options)
+
+       this.width = w
+       this.height = h
+       this.image = loadImage("assets/player.png")
+       
+       World.add(world,this.body)
+       
+   }
+
+   display(){
+
+       push()
+
+       translate(this.body.position.x,this.body.position.y)
+
+       rotate(this.body.angle)
+
+       imageMode (CENTER)
+
+       image(this.image,0,0,this.width,this.height)
+
+       pop()
+
+   }
+}
